6532504292dd7f11119e6edf04c7d5552d1d5fc1
priyankasrs <priyankasrs@gmail.com>
2019-01-31 21:58:17 -0800

    Adding docker files

--- services/appui/src/Ethereumsetup.js
+++ services/appui/src/Ethereumsetup.js
@@ -2,7 +2,7 @@ import Web3 from 'web3';
 
 const web3=new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 let ratingABI=[{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"ratingsReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"movieList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"movieNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[{"name":"movie","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"movie","type":"bytes32"}],"name":"voteForMovie","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]
-let ratingAddress='0x29fb5d78419284b914c163b992c6dd533b5b410a';
+let ratingAddress='0x6d211bfd3fbb3fbd7d006fcee797719821f9e05a';
 web3.eth.defaultAccount = web3.eth.accounts[0]
 
 
